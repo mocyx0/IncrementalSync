b212cba6b16929f028319b796bd212a5d8d07ac5
zsn423273 <291469736@qq.com>
2017-06-11 11:15:32 +0800

    通过文件位置进行重做，并进行了优化

--- src/main/java/org/pangolin/xuzhe/stringparser/Redo.java
+++ src/main/java/org/pangolin/xuzhe/stringparser/Redo.java
@@ -10,30 +10,6 @@ import static org.pangolin.xuzhe.stringparser.ReadingThread.getLineByPosition;
  * Created by ubuntu on 17-6-8.
  */
 public class Redo {
-//    public static Record redo(LocalLogIndex indexes, long pk) {
-//        List<LocalLogIndex.IndexEntry> logs = indexes.indexes.get(pk);
-//        Collections.sort(logs);
-//        Collections.reverse(logs);
-//        Record r = null;
-//        int count = 0;
-//        for(LocalLogIndex.IndexEntry index : logs) {
-//            String line = getLineByPosition(index.fileNo, index.position);
-//            Log log = Log.parser(line);
-//            if(log.op == 'D'){
-//                break;
-//            }
-//            if(count == 0) {
-//                r = Record.createFromLastLog(log, indexes);
-//            }else {
-//                r.update(Log.parser(line), indexes);
-//            }
-//            if(log.op == 'I'){
-//                break;
-//            }
-//            ++count;
-//        }
-//        return r;
-//    }
 
     public static Record redo(LocalLogIndex indexes, long pk) {
         List<LocalLogIndex.IndexEntry> logs = getIndexEntries(indexes, pk);  //获取主键对应list
