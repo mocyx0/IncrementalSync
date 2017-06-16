1846885b438c4e2f0fb03df88e6c8305b16cc55b
mocyx <yuanye15977@163.com>
2017-06-16 14:53:45 +0800

    重构

--- src/main/java/org/pangolin/yx/NetClientHandler.java
+++ src/main/java/org/pangolin/yx/NetClientHandler.java
@@ -5,6 +5,7 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import org.pangolin.yx.nixu.NXClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,100 +20,16 @@ import java.util.TreeMap;
  */
 public class NetClientHandler extends ChannelInboundHandlerAdapter {
     private static Logger logger = LoggerFactory.getLogger(Client.class);
-    RandomAccessFile raf;
 
-    NetClientHandler() {
-        try {
-            String path = Config.RESULT_HOME + "/" + Config.RESULT_NAME;
-            File f = new File(path);
-            if (f.exists()) {
-                f.delete();
-            }
-            raf = new RandomAccessFile(path, "rw");
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(0);
-        }
+
+    private static WorkerClient workerClient;
+
+
+    NetClientHandler() throws Exception {
+        workerClient = new NXClient();
 
     }
 
-
-    private void writeToFile(byte[] data) throws Exception {
-        int len = data.length;
-        if (data[data.length - 1] == 0) {
-            len = data.length - 1;
-        }
-        raf.write(data, 0, len);
-    }
-
-    private static class DumpBlock {
-        long pos;
-        int length;
-    }
-
-    private static int printLineCount = 0;
-
-    private void dumpToFile() throws Exception {
-        logger.info(String.format("start dump recv size %d", writeOff));
-
-        ByteBuffer bf = ByteBuffer.wrap(buffer);
-        bf.position(writeOff);
-
-        bf.flip();
-        TreeMap<Integer, ArrayList<DumpBlock>> blocks = new TreeMap<>();
-        while (true) {
-            int index = bf.getInt();
-            if (index == 0) {
-                break;
-            } else {
-                int seq = bf.getInt();
-                int size = bf.getInt();
-                if (!blocks.containsKey(index)) {
-                    blocks.put(index, new ArrayList<DumpBlock>());
-                }
-                ArrayList<DumpBlock> arr = blocks.get(index);
-                DumpBlock newBlock = new DumpBlock();
-                newBlock.pos = bf.position();
-                newBlock.length = size;
-                arr.add(newBlock);
-                if (size > 1000) {
-                    //logger.info(String.format("block size %d", bf.position() + size));
-                }
-
-
-                bf.position(bf.position() + size);
-                //logger.info(String.format("index %d ,seq %d , block size %d , new pos %d", index, seq, size, bf.position()));
-            }
-        }
-        //write to file
-        for (Integer i : blocks.keySet()) {
-            ArrayList<DumpBlock> arr = blocks.get(i);
-            for (DumpBlock block : arr) {
-                //打印一部分输出
-                if (printLineCount < Config.PRINT_RESULT_LINE) {
-                    String s = new String(buffer, (int) block.pos, block.length);
-                    String[] ss = s.split("\\n");
-                    int j = 0;
-                    while (j < ss.length && printLineCount < Config.PRINT_RESULT_LINE) {
-                        logger.info(ss[j]);
-                        printLineCount++;
-                        j++;
-                    }
-                }
-                //raf.write(buffer.array(), (int) block.pos, block.length);
-                raf.write(buffer, (int) block.pos, block.length);
-            }
-        }
-        logger.info(String.format("end dump,file size:%d", raf.length()));
-        raf.close();
-
-    }
-
-    //
-    private static int RECV_BUFF_SZIE = 1024 * 1024 * 512;
-    private static int packCount = 0;
-    private static byte[] buffer = new byte[RECV_BUFF_SZIE];
-    private static int writeOff = 0;
 
     // 接收server端的消息，并打印出来
     @Override
@@ -121,29 +38,8 @@ public class NetClientHandler extends ChannelInboundHandlerAdapter {
 
         ByteBuf result = (ByteBuf) msg;
         //logger.info(String.format("channelRead size:%d", result.readableBytes()));
-        int readlLen = result.readableBytes();
-        result.readBytes(buffer, writeOff, result.readableBytes());
-        writeOff += readlLen;
-        if (writeOff > 0 && buffer[writeOff - 1] == 0) {
-            dumpToFile();
-            ctx.channel().close().sync();
-            System.exit(0);
-        }
-        /*
-        if (buffer.position() > 0 && buffer.array()[buffer.position() - 1] == 0) {
-            dumpToFile();
-            ctx.channel().close().sync();
-            System.exit(0);
-        }
-        */
-//        System.out.println("Server said:" + new String(result1));
+        workerClient.onData(result, ctx);
         result.release();
-
-        //ctx.writeAndFlush("I have received your messages and wait for next messages");
-        //ctx.write(stringToBuffer("hi!"));
-        //ctx.channel().writeAndFlush(stringToBuffer("hi!"));
-
-
     }
 
     private static ByteBuf stringToBuffer(String msg) {
@@ -155,6 +51,8 @@ public class NetClientHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         logger.info("channelActive");
+
+        workerClient.onActive();
 
         String msg = "I am prepared to receive messages";
         ByteBuf encoded = ctx.alloc().buffer(4 * msg.length());
--- src/main/java/org/pangolin/yx/WorkerClient.java
+++ src/main/java/org/pangolin/yx/WorkerClient.java
@@ -1,7 +1,17 @@
 package org.pangolin.yx;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+
+import java.nio.ByteBuffer;
+
 /**
  * Created by yangxiao on 2017/6/16.
  */
 public interface WorkerClient {
+
+    void onActive() throws Exception;
+
+    void onData(ByteBuf data, ChannelHandlerContext ctx) throws Exception;
+
 }
